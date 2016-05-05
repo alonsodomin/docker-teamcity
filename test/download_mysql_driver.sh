e1c16870b6d1d260be0bdd08afe21e900fcd52cd
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-05-05 12:49:17 +0100

    Fix Docker linter errors

--- test/download_mysql_driver.sh
+++ test/download_mysql_driver.sh
@@ -3,19 +3,19 @@
 MYSQL_CONNECTOR_VERSION=5.1.35
 
 function download_driver() {
-    echo "Downloading MYSQL Java Driver..."
+  echo "Downloading MYSQL Java Driver..."
 
-    if [ -d /var/lib/teamcity/lib/jdbc ]; then
-        rm -f /var/lib/teamcity/lib/jdbc/mysql-*.jar
-    else
-        mkdir -p /var/lib/teamcity/lib/jdbc
-    fi
+  if [ -d /var/lib/teamcity/lib/jdbc ]; then
+      rm -f /var/lib/teamcity/lib/jdbc/mysql-*.jar
+  else
+      mkdir -p /var/lib/teamcity/lib/jdbc
+  fi
 
-    wget -nv http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-$MYSQL_CONNECTOR_VERSION.tar.gz \
-         -O /tmp/mysql-connector-java-$MYSQL_CONNECTOR_VERSION.tar.gz
-    tar zxf /tmp/mysql-connector-java-$MYSQL_CONNECTOR_VERSION.tar.gz -C /tmp
-    cp /tmp/mysql-connector-java-$MYSQL_CONNECTOR_VERSION/mysql-connector-java-$MYSQL_CONNECTOR_VERSION-bin.jar /var/lib/teamcity/lib/jdbc
-    rm -fR /tmp/*
+  wget -nv http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-$MYSQL_CONNECTOR_VERSION.tar.gz \
+       -O /tmp/mysql-connector-java-$MYSQL_CONNECTOR_VERSION.tar.gz
+  tar zxf /tmp/mysql-connector-java-$MYSQL_CONNECTOR_VERSION.tar.gz -C /tmp
+  cp /tmp/mysql-connector-java-$MYSQL_CONNECTOR_VERSION/mysql-connector-java-$MYSQL_CONNECTOR_VERSION-bin.jar /var/lib/teamcity/lib/jdbc
+  rm -fR /tmp/*
 }
 
 test -f /var/lib/teamcity/lib/jdbc/mysql-connector-java-$MYSQL_CONNECTOR_VERSION-bin.jar || download_driver
