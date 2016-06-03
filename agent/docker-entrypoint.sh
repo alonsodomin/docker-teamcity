1e2719d2fa0f05cdc3bf9d95ffd0430dd7feb667
Arthur <art.enikoff@gmail.com>
2016-06-03 14:54:26 +0300

    alonsodomin/docker-teamcity#1 fixes for broken startscript

--- agent/docker-entrypoint.sh
+++ agent/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ ! -z "$TEAMCITY_PORT_8111_TCP_ADDR" ] && [ ! -z "$TEAMCITY_PORT_8111_TCP_PORT" ]; then
-  sed -n "s/<teamcity_server>/$TEAMCITY_PORT_8111_TCP_ADDR" /etc/teamcity-agent/buildAgent.properties
-  sed -n "s/<teamcity_port>/$TEAMCITY_PORT_8111_TCP_PORT" /etc/teamcity-agent/buildAgent.properties
+  sed -i "s/<teamcity_server>/"${TEAMCITY_PORT_8111_TCP_ADDR}"/" /etc/teamcity-agent/buildAgent.properties
+  sed -i "s/<teamcity_port>/"$TEAMCITY_PORT_8111_TCP_PORT"/" /etc/teamcity-agent/buildAgent.properties
 fi
 
-exec "$TEAMCITY_AGENT_HOME/bin/agent.sh run"
+exec "$TEAMCITY_AGENT_HOME/bin/agent.sh" run
